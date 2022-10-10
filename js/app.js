8c065b6df0ddb72b2c81a4b688c9fb45a3a711f2
Keyan <keyantabor98@gmail.com>
2022-10-10 02:00:34 -0400

    remaining questions

--- js/app.js
+++ js/app.js
@@ -56,11 +56,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sumQ3 = a + b + c;
+  let multiQ3 = a * b * c;
+  let message1Q3 = `${a} and ${b} and ${c} sum to ${sumQ3}.`;
+  let message2Q3 = `The product of ${a} and ${b} and ${c} is ${multiQ3}.`;
+  return [sumQ3, multiQ3, message1Q3, message2Q3];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -78,12 +82,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sumTotal1 = sum(sumArr)[0, 1];
+  let sumTotal2 = sum(sumTotal1, sumArr)[2];
+  let sumTotal = sum(sumTotal1, sumTotal2);
+  let messageQ4 = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumTotal} is their sum.`;
+  return [sumTotal, messageQ4];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
